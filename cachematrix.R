3263e3148ac963bffc990fc877b82cf8af9a5cec
WindZhang <wind,zhang@139.com>
2015-04-21 13:28:31 +0800

    Ready for peer assignment

--- cachematrix.R
+++ cachematrix.R
@@ -3,7 +3,18 @@
 ## This R code is used for the peer assignment of Rprog-013.
 
 ## This function creates a special "matrix" object that can cache its inverse.
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = matrix()){
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(solve) m <<- solve
+        getinverse <- function() m
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 
 }
 
@@ -15,4 +26,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        ## If not found the inverse in the cache, then computes it and save in the cache
+        data <- x$get()
+        m <- solve(data)
+        x$setinverse(m)
+        m
+
 }
