7d55ef6b9852fb82cd30d1428f476261ca4a144d
WindZhang <wind,zhang@139.com>
2015-04-21 10:39:58 +0800

    Committ for peer assignment
    Ready for peer assignment.

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,17 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This funtion is to cache the inverse of square matrix. Please note:
+## We assume the matrix supplied is always invertible.
+## This R code is used for the peer assignment of Rprog-013.
 
-## Write a short comment describing this function
-
+## This function creates a special "matrix" object that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by 
+## makeCacheMatrix above. If the inverse has already been calculated 
+## (and the matrix has not changed), then cacheSolve should retrieve 
+## the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
